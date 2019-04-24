6e48f709b828d502144b2a3c421d2cd9983a15db
jmarzin <jacques.marzin@free.fr>
2019-04-24 12:45:45 +0700

    final

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,29 +1,90 @@
-##
-# le fichier de paramétrage de nginx est dans /etc/nginx (modifiable sous root)
-# le fichier de logs de nginx est dans /var/logs/nginx (accessible sous root)
-# pour migrer la base faire       RAILS_ENV=production rails db:migrate
-# pour compiler les assets faire  RAILS_ENV=production rails assets:precompile
-# pour lancer le serveur faire    bundle exec puma -e production -d --pidfile RUNNINGPID
-# pour relancer le serveur faire  kill -s SIGUSR2 `cat RUNNINGPID`
-# pour arrêter le serveur faire   kill -s SIGTERM `cat RUNNINGPID`
+# Change these
+server '54.36.98.168', port: 22, roles: [:web, :app, :db], primary: true
+
+set :repo_url,        'https://github.com/jmarzin/msm.git'
+set :application,     'msm'
+set :user,            'deploy'
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+
+# Don't change these unless you know what you're doing
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 set :format, :pretty
-set :repo_url, 'https://github.com/jmarzin/msm.git'
+set :log_level, :info
 
-set :deploy_to, '/home/deploy/msm'
+## Defaults:
+# set :scm,           :git
+# set :branch,        :master
+# set :format,        :pretty
+# set :log_level,     :debug
+# set :keep_releases, 5
 
+## Linked Files & Directories (Default None):
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+#
 set :linked_files, %w{config/master.key}
 set :linked_files, %w{config/database.yml public/agenda.txt}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/assets public/system public/images public/gpx public/materiels}
 
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
+
 namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
 
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
+      invoke 'puma:restart'
     end
   end
 
-  after :publishing, 'deploy:restart'
-  after :finishing, 'deploy:cleanup'
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
 end
+
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
